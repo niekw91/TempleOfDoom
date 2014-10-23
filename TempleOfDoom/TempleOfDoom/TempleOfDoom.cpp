0443d7cc52d2d967309d4d4ffb3c759fcdc7f32c
eihi <etigele@gmail.com>
2014-10-23 09:26:27 +0200

    Check for memory leaks

--- TempleOfDoom/TempleOfDoom/TempleOfDoom.cpp
+++ TempleOfDoom/TempleOfDoom/TempleOfDoom.cpp
@@ -1,11 +1,17 @@
 // TempleOfDoom.cpp : Defines the entry point for the console application.
 //
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
 
 #include "stdafx.h"
 #include "Game.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	// Check for memory leaks
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
 	// Set window size
 	system("MODE CON: COLS=80 LINES=40");
 
@@ -16,11 +22,11 @@ int _tmain(int argc, _TCHAR* argv[])
 	game.Init();
 
 	// Start game
-	game.Start();
+	//game.Start();
 
 	// Cleanup game
 	game.Cleanup();
-
+	//_CrtDumpMemoryLeaks();
 	return 0;
 }
 
